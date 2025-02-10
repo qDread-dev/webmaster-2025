a56d09f1b9803b092796b54c6e3c955b79b239db
Nik-Sharp <96154725+Nik-Sharp@users.noreply.github.com>
2025-02-09 20:57:29 -0500

    Home Page code
    - TODO: make footer
    
    code is awful, i know, but it works (barely)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: "https://qdread-dev.github.io/",
-  base: "https://github.com/qdread-dev/webmaster-2025"
+  base: "https://github.com/qdread-dev/webmaster-2025",
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
