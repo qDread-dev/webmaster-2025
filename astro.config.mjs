2f2acbed3b5aefa3bd8359ad3fd398b38f0f7c53
Nik-Sharp <96154725+Nik-Sharp@users.noreply.github.com>
2025-02-09 22:29:29 -0500

    Should work

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   integrations: [react()],
   site: "https://qdread-dev.github.io/",
-  base: "/",
+  base: "/webmaster-2025",
 
   vite: {
     plugins: [tailwindcss()]
